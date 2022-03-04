16ccdcc5aacf86d7b96bd4ee484d114209afd5bf
connord28 <60565612+connord28@users.noreply.github.com>
2022-03-04 11:50:32 -0800

    Delete Library directory

--- Library/PackageCache/dev.yarnspinner.unity@2.0.2/Editor/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: InternalsVisibleTo("YarnSpinner.Editor.Tests")]
-
-[assembly: AssemblyFileVersion("2.0.2.0")]
-[assembly: AssemblyInformationalVersion("2.0.2-development")]
